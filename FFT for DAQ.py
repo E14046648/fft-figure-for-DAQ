baf88f009bc215c6cb078cdcfb17629d648ce153
Yang Hang Wu <39996866+YangHangWu@users.noreply.github.com>
2019-12-05 20:42:12 +0800

    add historical max freq plot

--- FFT for DAQ.py
+++ FFT for DAQ.py
@@ -2,7 +2,7 @@
 ========================================================
 | Title:  time domain and frequenct domain(fft) figure |
 | Author: Yang Hang Wu                                 |
-| Date:   22 Nov 2019                                  |
+| Date:   25 Nov 2019                                  |
 ========================================================
 '''
 
@@ -12,9 +12,18 @@ import time
 import matplotlib
 import matplotlib.pyplot as plt
 import glob, os
-
+import threading
 import seaborn
 from scipy.fftpack import fft,ifft
+# 子執行緒的工作函數
+
+def plot_job():
+    for n in range(channel_num):
+        axs[n, 0].clear()          
+        axs[n, 0].plot(np.arange(0,0.4,1/1000),channels[n], color[n])
+        axs[n, 0].set_title('channel '+str(n+1)+' time domain')
+    print("thread finished")
+    
 
 def move_figure(f, x, y):
     """Move figure's upper left corner to pixel (x, y)"""
@@ -29,25 +38,39 @@ def move_figure(f, x, y):
         f.canvas.manager.window.move(x, y)
 
 
+Fs = 25600.0;                   # sampling rate (Hz)
+T = 1;                          # sampling time (s)
 interval=1                      # interval of plotting data
-time=0.5                          # update time of plotting rate
+pause_time=0.5                  # update time of plotting rate
 fig_w=10                        # fig width
 fig_h=5                         # fig hight
 fig_x=130                       # fig upper left x 
-fig_y=80                       # fig upper left y
-color=['go-','ro-','bo-','co-'] # colors of time domain subplot
+fig_y=80                        # fig upper left y
+color=['g-','r-','b-','c-']     # colors of time domain subplot
 f_color=['g-','r-','b-','c-']   # colors of frequenct domain subplot
-Fs = 1000.0;                    # sampling rate (Hz)
-T = 0.4;                        # sampling time (s)
+
 
 path = os.getcwd()
 os.chdir(path)
 print(path)
-fig, axs = plt.subplots(4, 2,figsize = (fig_w, fig_h))
-move_figure(fig, fig_x, fig_y)
 index=0
+tStart = time.time()#計時開始
+fig, axs = plt.subplots(4, 3,figsize = (fig_w, fig_h))
+move_figure(fig, fig_x, fig_y)
+tEnd = time.time()#計時結束
+print ("create fig cost %f sec" % (tEnd - tStart))
+'''
+file_thread = threading.Thread(target = create_fig)
+file_thread.start()
+plot_thread_pool=[]
+for i in range(8):
+    plot_thread_pool.append(threading.Thread(target = plot_job))
+'''
+file_num=0
+max_freq_channels=[[],[],[],[]]
 while(True):
     '''get files in dirctory'''
+    tStart = time.time()#計時開始
     file_list=[]
     while(True):
         for file in glob.glob("*.lvm"):
@@ -63,48 +86,73 @@ while(True):
         print(index)
 
     else:
-        print("no new file")
+        #print("no new file")
         continue
     
     if(index%interval==0):
         '''read file'''
+        file_num+=1
+        channels=[[],[],[],[]]
         file_path = os.path.join(path, "exhibition_"+str(index)+".lvm" )
         print(file_path)
-        channels=[[],[],[],[]]
         fp = open(file_path, "r")
-        channel_num=0
+        global channel_num
         for line in iter(fp):
             components=line.split("	")
             channel_num=len(components)-1
             for k in range(channel_num):                
                 channels[k].append(float(components[k+1]))
         fp.close()
+        tEnd = time.time()#計時結束
+        print ("readfile cost %f sec" % (tEnd - tStart))
 
         ''' fft'''
+        tStart = time.time()#計時開始
         freq_channels=[[],[],[],[]]
+
         for i in range(channel_num):
             freq_channels[i]=abs(fft(channels[i]))
+            freq_channels[i][:] = [x / Fs for x in freq_channels[i]]
             freq_channels[i]=freq_channels[i][range(int(Fs*T/2))]
+            max_freq_channels[i].append(list(freq_channels[i]).index(max(freq_channels[i])))
+            print(max_freq_channels[i])
         xf = np.arange(len(freq_channels[channel_num-1]))
         xf = xf[range(int(Fs*T/2))] #half interval
+        tEnd = time.time()#計時結束
+        print ("fft cost %f sec" % (tEnd - tStart))
 
         '''plotting'''
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=None, hspace=1)
         fig.suptitle("exhibition_"+str(index)+".lvm", fontsize=16)
-        mngr = plt.get_current_fig_manager()       
+        mngr = plt.get_current_fig_manager()  
+        #plot_t=threading.Thread(target = plot_job)
+        tStart = time.time()#計時開始
+        #plot_t.start()
+        
         for n in range(channel_num):
             axs[n, 0].clear()          
             axs[n, 0].plot(np.arange(0,T,1/Fs),channels[n], color[n])
             axs[n, 0].set_title('channel '+str(n+1)+' time domain')
+        
+        #tEnd = time.time()#計時結束
+        #print ("plot time cost %f sec" % (tEnd - tStart))
+        #tStart = time.time()#計時開始 
         for n in range(channel_num):
             axs[n, 1].clear()          
-            axs[n, 1].plot(xf,freq_channels[n], f_color[n])
+
+            axs[n, 1].plot(xf[:1000],freq_channels[n][:1000], f_color[n])
             axs[n, 1].set_title('channel '+str(n+1)+' frequency domain')
+        for n in range(channel_num):
+            axs[n, 2].clear()          
+            axs[n, 2].scatter(range(file_num),max_freq_channels[n])
+            axs[n, 2].set_title('channel '+str(n+1)+' max frequency')
+        #plot_t.join()
+        tEnd = time.time()#計時結束
+        print ("plot freq cost %f sec" % (tEnd - tStart))
         try:
-            plt.pause(time)
+            plt.pause(pause_time)
         except:
             fig, axs = plt.subplots(4, 2,figsize = (fig_w, fig_h))
             move_figure(fig, fig_x, fig_y)
-
 
